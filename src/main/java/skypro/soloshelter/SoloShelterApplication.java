7bf6a62287bc8f2667be56966b5660afee6150f3
Hybusa <alkouzne007@gmail.com>
2023-06-05 14:52:03 +0300

    initial commit

---
+++ src/main/java/skypro/soloshelter/SoloShelterApplication.java
@@ -0,0 +1,13 @@
+package skypro.soloshelter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloShelterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloShelterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/skypro/soloshelter/SoloShelterApplicationTests.java
@@ -0,0 +1,13 @@
+package skypro.soloshelter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloShelterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
